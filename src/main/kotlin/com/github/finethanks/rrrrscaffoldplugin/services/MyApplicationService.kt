ea9d5095ad530eca26e89bc5ea04cc51a81e5473
GitHub Action <action@github.com>
2022-01-10 16:47:43 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/finethanks/rrrrscaffoldplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.finethanks.rrrrscaffoldplugin.services
+
+import com.github.finethanks.rrrrscaffoldplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/finethanks/rrrrscaffoldplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/finethanks/rrrrscaffoldplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.finethanks.rrrrscaffoldplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.finethanks.rrrrscaffoldplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
